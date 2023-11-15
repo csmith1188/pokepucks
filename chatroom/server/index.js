d3a3ec702cff667429bf82b7d0d6dd4360076cc9
Brand24376 <82458664+Brand24376@users.noreply.github.com>
2023-11-15 14:57:34 -0500

    Made progress on the chatroom

---
+++ chatroom/server/index.js
@@ -0,0 +1,26 @@
+/****************************** Script Header ******************************\
+Project: PokePucks
+Author: Brandon Camacho
+Editors: Brandon Camacho
+
+<Description>
+Filler
+\***************************************************************************/
+/*
+// A list for all the required node modules to install
+// Make sure terminal is in the server folder
+npm i ws
+*/
+
+const ws = require('ws'); // Makes a dependency for the websockets module
+const server = new ws.Server({ port: '3000' }); // Makes a new server which runs on port 3000
+
+// Once a connection is established, listen for a socket
+server.on('connection', socket => {
+    // Listens for a message
+    socket.on('message', message => {
+        // Logs and sends the same message back
+        console.log(message);
+        socket.send(`${message}`);
+    });
+});
