986793d76432585ba5667d78fb91ec7859a374a7
Brand24376 <82458664+Brand24376@users.noreply.github.com>
2023-11-28 11:17:39 -0500

    continued working on the chatroom

--- chatroom/server/index.js
+++ chatroom/server/index.js
@@ -4,25 +4,50 @@ Author: Brandon Camacho
 Editors: Brandon Camacho
 
 <Description>
-Filler
+Code for the backend server-side for the chatroom.
 \***************************************************************************/
 /*
 // A list for all the required node modules to install
 // Make sure terminal is in the server folder
-npm i ws
-// Start the server by using 'npm start' or live server on /chatroom/app/index.html
+npm i socket.io
+npm i express
+npm i -D nodemon (Not required but makes editing this file much more convenient. Nodemon will restart the server automatically every time you save the server.)
+// Start the server by using 'npm start' while in the server folder
 */
 
-const ws = require('ws'); // Makes a dependency for the websockets module
-const server = new ws.Server({ port: '3000' }); // Makes a new server which runs on port 3000
+import express from 'express';
+import { Server } from "socket.io";
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const PORT = process.env.PORT || 3500;
+
+const app = express(); // Our express server is referred to as app
+
+app.use(express.static(path.join(__dirname, "public"))); // Defines our static folder so when we get a request to the root domain, it will send eveything to the public directory that will contain the static assets
+
+const expressServer = app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+});
+
+// Grabs the server imported from socket.io, gives it the expressServer, and gives it options
+const io = new Server(expressServer, {
+    cors: {
+        // origin allows you to change what is accepted and what is blocked
+        origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5500", "http://127.0.0.1:5500"] // Looks at the node environment. If the node environemnt equals production, origin is set to false because we don't want anyone outside of the domain the server is currently on to access it. If it doesn't equal production, origin is set to the address that we will allow to access our socket.io server
+    }
+})
 
 // Once a connection is established, listen for a socket
-server.on('connection', socket => {
+io.on('connection', socket => {
+    console.log(`User ${socket.id} connected`);
+
     // Listens for a message
-    socket.on('message', message => {
-        const b = Buffer.from(message);
-        // Logs and sends the same message back
-        console.log(message);
-        socket.send(`${message}`);
+    socket.on('message', data => {
+        console.log(data);
+        io.emit('message', `${socket.id.substring(0, 5)}: ${data}`); // Emits the message to everyone that is connected to the server
     });
 });
--- chatroom/server/public/app.js
+++ chatroom/server/public/app.js
@@ -4,35 +4,40 @@ Author: Brandon Camacho
 Editors: Brandon Camacho
 
 <Description>
-Filler
+Code for the frontend serer-side for the chatroom.
 \***************************************************************************/
 
 // Defines socket = to a new websocket
-const socket = new WebSocket('ws://localhost:3000');
+const socket = io('ws://localhost:3500');
+
+const activity = document.querySelector('.activity'); // Selects anything with the activity class in index.html
+const msgInput = document.querySelector('input');
 
 // Function used to send a message
 function sendMessage(e) { //sendMeassage recieves an event which is represented with the letter e\
     //Allows you to submit the form without reloading the page
     e.preventDefault();
-    // Defines the input and grabs it from the html
-    const input = document.querySelector('input');
     // If input has a value
-    if (input.value) {
-        // Send the value of the input
-        socket.send(input.value);
-        // Replace the input with nothing
-        input.value = "";
+    if (msgInput.value) {
+        // Emits the value of the msgInput
+        socket.emit('message', msgInput.value);
+        // Replace the msgInput with nothing
+        msgInput.value = "";
     };
-    // Puts the focus back on the input
-    input.focus();
+    // Puts the focus back on the msgInput
+    msgInput.focus();
 };
 
 // Selects the form
 document.querySelector('form').addEventListener('submit', sendMessage); // listens for the submit event of the form and calls the sendMessage function
 
 // Listen for messages
-socket.addEventListener('message', ({ data }) => {
+socket.on('message', (data) => {
     const li = document.createElement('li');
     li.textContent = data;
     document.querySelector('ul').appendChild(li);
 });
+
+msgInput.addEventListener('keypress', () => {
+    socket.emit('activity', socket.id.substring(0, 5))
+}); // stopped tutorial at 4:02 while writing this code
