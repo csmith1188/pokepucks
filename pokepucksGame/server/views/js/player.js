c5644a262e243d4eef27186e64d952bc498d1d14
Brand24376 <82458664+Brand24376@users.noreply.github.com>
2024-02-08 10:03:15 -0500

    Fixed Issue #72

--- pokepucksGame/server/views/js/player.js
+++
@@ -1,9 +0,0 @@
-class Player {
-    constructor(hp, power, prize, attack,slammer) {
-        this.hp = hp;
-        this.power = power;
-        this.prize = prize;
-        this.attack = attack
-        this.slammer = slammer
-    }
-}
