76bfa67b9057e711519c73e1a045800a9c12312f
Logan-cruz <79530781+Logan-cruz@users.noreply.github.com>
2024-02-15 13:43:27 -0500

    Update app.js

--- pokepucksGame/server/views/app.js
+++ pokepucksGame/server/views/app.js
@@ -8,11 +8,11 @@ Code for the frontend side for the PokePucks game.
 \***************************************************************************/
 // Chatroom Code
 // Define the urls
-const ROOT_URL = 'http://172.16.3.111:3000/'; // 'http://ipAddressOfThisServer:port/';
-const LOGIN_URL = 'http://172.16.3.111:3000/login'; // 'http://ipAddressOfThisServer:port/login';
-const LOGOUT_URL = 'http://172.16.3.111:3000/logout'; // 'http://ipAddressOfThisServer:port/logout';
-const LOBBY_URL = 'http://172.16.3.111:3000/lobby'; // 'http://ipAddressOfThisServer:port/lobby';
-const CHATROOM_URL = 'http://172.16.3.111:3000/chatroom'; // 'http://ipAddressOfThisServer:port/chatroom';
+const ROOT_URL = 'http://fillerIp:3000/'; // 'http://ipAddressOfThisServer:port/';
+const LOGIN_URL = 'http://fillerip:3000/login'; // 'http://ipAddressOfThisServer:port/login';
+const LOGOUT_URL = 'http://fillerIP:3000/logout'; // 'http://ipAddressOfThisServer:port/logout';
+const LOBBY_URL = 'http://FillerIp:3000/lobby'; // 'http://ipAddressOfThisServer:port/lobby';
+const CHATROOM_URL = 'http://FillerIP:3000/chatroom'; // 'http://ipAddressOfThisServer:port/chatroom';
 
 // Defines socket = to a new websocket
 const socket = io(ROOT_URL);
